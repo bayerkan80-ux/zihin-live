48c57ef2c5c2ef54f4d7d7b514d1fefe97b7c2be
bayerkan80-ux <bayerkan80@gmail.com>
2025-11-26 18:16:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    ZIHIN_LIVE_API_KEY: process.env.ZIHIN_LIVE_API_KEY,
+  },
+};
+
+module.exports = nextConfig;
